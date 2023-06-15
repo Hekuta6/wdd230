81e128ff0e59048a90e23f17012765fd6bf8a1cf
[Hector Rodriguez] <hekuta.rod@gmail.com>
2023-06-15 15:47:51 -0700

    Web Design Principles activity

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -1,4 +1,5 @@
-var currentYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = currentYear;
-var lastModified = document.lastModified;
-document.getElementById("lastModified").innerHTML = 'Last Modification: ' + lastModified;
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', { month: "2-digit", day: "2-digit", year: "numeric" });
+let time = lastModified.toLocaleString('en-GB', { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastModified").innerHTML = dateTime;
