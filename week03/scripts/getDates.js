4691eb96f118dabf95832966496644043ffba2f1
[Hector Rodriguez] <hekuta.rod@gmail.com>
2023-06-22 22:45:50 -0700

    Activities
    Update

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,7 @@
+todaysDate = new Date(document.lastModified);
+dayName = todaysDate.getDate();
+monthName = todaysDate.getMonth() + 1;
+year = todaysDate.getFullYear();
+time = todaysDate.toLocaleTimeString('it-IT');
+document.querySelector('#year').textContent = year;
+document.querySelector('#lastModified').textContent = `Last Modification: ${dayName}/${monthName}/${year} ${time}`;
