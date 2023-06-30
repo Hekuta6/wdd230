c8699e7f608761b7d34b0ff7212bcf1280141519
[Hector Rodriguez] <hekuta.rod@gmail.com>
2023-06-29 20:58:02 -0700

    Week 4

---
+++ week04/scripts/main.js
@@ -0,0 +1,33 @@
+
+const darkModeButton = document.getElementById('darkModeButton');
+const main = document.querySelector('main');
+
+darkModeButton.addEventListener('click', () => {
+    if (darkModeButton.textContent.includes("☑️")) {
+        main.classList.toggle('dark-mode');
+        darkModeButton.textContent = "❎";
+    } else {
+        main.classList.toggle('dark-mode');
+        darkModeButton.textContent = "☑️";
+    }
+
+});
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
+
+
+
