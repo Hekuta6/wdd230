f6cbb263ab81ef168d89ed64f651535114a9db08
[Hector Rodriguez] <hekuta.rod@gmail.com>
2023-07-08 12:26:51 -0700

    update

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,48 @@
+const url = 'https://hekuta6.github.io/wdd230/chamber/data/members.json';
+const cards = document.querySelector('#cards');
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.members);
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let address = document.createElement('p')
+        let website = document.createElement('a')
+        let membership = document.createElement('p');
+
+        fullName.textContent = `${member.name}`;
+        console.log(fullName)
+
+        address.textContent = `Address: ${member.address}`;
+        console.log(website);
+
+        membership.textContent = `${member.membershipLevel} Member`
+
+
+        portrait.setAttribute('src', member.image);
+        portrait.setAttribute('alt', `${member.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '500');
+        portrait.setAttribute('height', '250');
+
+        website.setAttribute('href', member.website);
+        website.textContent = `${member.website}`;
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(address);
+        card.appendChild(website);
+        card.appendChild(membership);
+        cards.appendChild(card);
+    });
+}
+
+getMemberData();
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -27,51 +27,5 @@ numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
 
-if (page == "directory.html") {
-    memberFetch()
-    const gridButton = document.querySelector("#grid");
-    const display = document.querySelector("#members");
-    const icon = document.querySelector("#icon");
-    const icon2 = document.querySelector("#icon2")
-
-    gridButton.addEventListener("click", () => {
-        if (display.classList == "list") {
-            display.classList.add("grid");
-            display.classList.remove("list");
-            icon.classList.add("fa-table-list")
-            icon.classList.remove("fa-grip-vertical");
-            icon2.classList.remove("fa-grip-vertical");
-        } else {
-            display.classList.add("list");
-            display.classList.remove("grid");
-            icon.classList.remove("fa-table-list")
-            icon.classList.add("fa-grip-vertical");
-            icon2.classList.add("fa-grip-vertical");
-        }
-    });
-}
-
-function displayPopUp() {
-    let day = Date(dt).toString();
-    let weekday = day[0] + day[1] + day[2];
-    if (weekday == "Mon" || weekday == "Tue" || weekday == "Wed") {
-        bruce.classList.remove("hidden");
-        bruce.classList.add("shown")
-    }
-}
-
-if (page == "index.html") {
-    apiFetch();
-    memberFetch();
-    const hulk = document.getElementById("banner");
-    const bruce = document.getElementById("bruce");
-    displayPopUp();
-    hulk.addEventListener("click", () => {
-        bruce.classList.remove("shown")
-        bruce.classList.add("hidden");
-    });
-}
-
-
 
 
